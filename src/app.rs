375032ce7d1f9631b7d9bb604d9a2b7da0446780
b4-io <brunodesimone17@gmail.com>
2023-01-31 19:32:45 -0300

    simple example

--- src/app.rs
+++ src/app.rs
@@ -21,48 +21,56 @@ pub fn App(cx: Scope) -> impl IntoView {
         <Router>
             <main>
                 <Routes>
-                    <Route path="" view=|cx| view! { cx, <HomePage/> }/>
+                    <Route path="/" view=|cx| view! { cx,
+                        <HomePage/>
+                     }/>
+                    <Route path="/simple" view=|cx| view! { cx,
+                        <HomePage2/>
+                     }/>
                 </Routes>
             </main>
         </Router>
     }
 }
 
-async fn fetch_cats() -> Option<Vec<String>> {
+async fn _fetch_cats() -> Option<Vec<String>> {
     Some(vec!["da".to_string()])
 }
 
-/// Renders the home page of your application.
+/// WITH THIS IT RENDERS AGAIN
 #[component]
 fn HomePage(cx: Scope) -> impl IntoView {
     // Creates a reactive value to update the button
     let counter = create_rw_signal(cx, 1);
-
-    let items_resource = create_resource(cx, move || counter.get(), move |_| fetch_cats());
+    let items_resource = create_resource(cx, move || counter.get(), move |_| _fetch_cats());
 
     view! { cx,
-        <h1>"Welcome to Leptos!"</h1>
+        <A href="/simple">"Simple"</A>
+        <h1>"Double render error with hydration error!"</h1>
         <button on:click=move |_| counter.update(|n| *n += 1)>"Refresh"</button>
         <Suspense fallback=move || view! { cx, <p>"Loading (Suspense Fallback)..."</p> }>
             {move || {
                 items_resource.read().map(|data| match data {
-                    None => view! { cx,  <><pre>"Error"</pre></>},
-                    Some(fetched_items) => view! { cx, <>
-                        {
-                            fetched_items.iter()
-                            .map(|_src| {
-                                view! { cx,
-                                    <p>"dada"</p>
-                                }
-                            })
-                            .collect::<Vec<_>>()
-                        }
-                    </>
+                    None => view! { cx,  <p>"Error"</p>},
+                    Some(_fetched_items) => view! { cx,
+                        <p>"dada"</p>
                     },
                 })
             }}
         </Suspense>
 
 
+    }
+}
+
+/// WITH THIS ITS JUST HYDRATION ERROR
+#[component]
+fn HomePage2(cx: Scope) -> impl IntoView {
+    view! { cx,
+        <A href="/">"Double render with hydration error"</A>
+        <h1>"Simple Hydration error!"</h1>
+        <Suspense fallback=move || view! { cx, <p>"Loading (Suspense Fallback)..."</p> }>
+            <p>"asdafasfaf"</p>
+        </Suspense>
     }
 }
